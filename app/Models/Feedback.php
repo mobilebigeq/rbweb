d694217acb65110b723a64af55bff1dfabe2ea9e
andrew3008 <nranjithnadar.gmail.com>
2017-06-10 19:54:54 +0530

    commit feedback api controller

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -8,7 +8,7 @@ class Feedback extends Model
 {
     protected $table = 'feedback';
 
-    protected $fillable = ['city','health_card_no','uhid_no','name','age','mobile','email','company','area','zip','category'];
+    protected $fillable = ['health_problem','experience_with_doctor','doctor_friendliness','value_for_money','explanation_of_health_issue','treatment_satisfaction','doctor_recommend','patient_name','patient_id','patient_mobile','submit_as_anonymously'];
 
     protected function create($params)
     {
